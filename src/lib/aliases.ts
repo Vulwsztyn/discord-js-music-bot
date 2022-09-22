df16fbfe0e482b3615c14406b5b9e357c615e87e
Artur Mostowski <vulwsztyn@gmail.com>
2022-09-22 20:54:59 +0200

    pt & playtop

--- src/lib/aliases.ts
+++ src/lib/aliases.ts
@@ -2,6 +2,8 @@ export const aliases: Record<string, string> = {
     q: "queue",
     p: "play",
     pn: "playnext",
+    pt: "playnext",
+    playtop: "playnext",
     s: "skip",
     fs: "skip",
     forceskip: "skip",
